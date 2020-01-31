130377f1f2fe85a2786b940547991a2541f90825
shengxinjing <316783812@qq.com>
2020-01-31 15:55:18 +0800

    web-worker生成哈希

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
+    <h1>测试</h1>
     <input type="file" @change="handleFileChange" />
     <el-button type="primary" @click="handleUpload">上传</el-button>
 
     <div>
+      <div>计算文件 hash</div>
+      <el-progress :percentage="hashProgress"></el-progress>
       <div>总进度</div>
       <el-progress :percentage="uploadPercentage"></el-progress>
       <el-table :data="chunks">
@@ -32,16 +35,23 @@
 
 <script>
 import { request } from "./util";
-import axios from 'axios'
-// ~@todo axios发送请求  
+import SparkMd5 from 'spark-md5'
+// import axios from 'axios'
+
+// @todo size切分
 // @todo canvas方块进度条
 // @todo 并发量控制+报错重试
-// @todo 上传失败文件碎片的清理工作（垃圾文件每天清理）
-// @todo 大文件的hash值计算 （三分+名字+size）或者每10M三分 节省时间 第一块和最后一块
-// @todo 计算哈希 使用time-slice
-const $axios = axios.create({
-  baseURL: 'http://localhost:3000',
-})
+// @todo 上传失败文件碎片的清理工作
+// @todo 大文件的hash值计算 （三分+名字+size）或者每10M三分 节省时间 第一块和最后一块取全部，中间的抽样
+// @todo 计算哈希 使用time-slice (setTimeout和requestIdleCallback)
+// @todo 哈希的计算分级，先计算前面2M和最后一块 中间取样的，如果不同直接上传，想通再计算全部的
+
+// @todo size大小动态调配 根据网速，慢启动
+
+
+// const request = axios.create({
+//   baseURL: 'https://some-domain.com/api/',
+// })
 
 const SIZE = 3 * 1024 * 1024;
 export default {
@@ -49,7 +59,8 @@ export default {
     container: {
       file: null
     },
-    chunks: []
+    chunks: [],
+    hashProgress:0
   }),
   filters: {
     transformByte(val) {
@@ -80,6 +91,8 @@ export default {
         chunks.push({ file: file.slice(cur, cur + size) });
         cur += size;
       }
+      console.log(chunks)
+      console.log(chunks.map(v=>v.file))
       return chunks;
     },
     async uploadChunks() {
@@ -91,11 +104,13 @@ export default {
           form.append("filename", this.container.file.name);
           return {form,index};
         })
-        .map(({form,index}) => $axios.post('/upload',form, {
-          onUploadProgress:this.createProgresshandler(this.chunks[index])
+        .map(({form,index}) => request({ 
+          url: "http://localhost:3000/upload", 
+          data: form,
+          onProgress:this.createProgresshandler(this.chunks[index])
         }))
-        
       const ret = await Promise.all(list)
+      console.log(ret)
       await this.mergeRequest();
     },
     createProgresshandler(item){
@@ -105,19 +120,39 @@ export default {
       }
     },
     async mergeRequest() {
-      await $axios.post('/merge',{
+      await request({
+        url: "http://localhost:3000/merge",
+        headers: {
+          "content-type": "application/json"
+        },
+        data: JSON.stringify({
           filename: this.container.file.name,
           size:SIZE
         })
-
+      });
+    },
+    async calculateHash(chunks){
+      return new Promise(resolve=>{
+        // web-worker 防止卡顿主线程
+        this.container.workder = new Worker('/hash.js')
+        this.container.workder.postMessage({chunks})
+        this.container.workder.onmessage = e=>{
+          const {progress, hash} = e.data
+          this.hashProgress = Number(progress.toFixed(2))
+          if(hash){
+            resolve(hash)
+          }
+        }
+      })
     },
     async handleUpload() {
       if (!this.container.file) return;
-      console.log(Object.keys(this.container.file))
-      // return 
       const chunks = this.createFileChunk(this.container.file);
 
+      // 计算哈希
+      this.container.hash = await this.calculateHash(chunks)
       this.chunks = chunks.map((chunk, index) => ({
+        fileHash: this.container.hash,
         chunk: chunk.file,
         index,
         hash: this.container.file.name + "-" + index,
